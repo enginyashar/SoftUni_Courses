0e25ff51c8913439da53798a62ea97c4059146b1
enginyashar <114264911+enginyashar@users.noreply.github.com>
2023-08-09 09:32:36 +0300

    Create 1. Student Information.cs

---
+++ CSharp/Fundamentals/1. Student Information.cs
@@ -0,0 +1,23 @@
+//Microsoft (R) Visual C# Compiler version 3.4.0-beta4-19562-05 (ff930dec)
+//Copyright (C) Microsoft Corporation. All rights reserved.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:f2}");
+        }
+    }
+}
